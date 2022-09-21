8330fefa6339721c33de90190533ebe8c22eadda
Артём Ванин <v.artyo@mail.ru>
2022-09-21 20:44:21 +0300

    second lab

--- Laba2/l2/l2/l2.cpp
+++ Laba2/l2/l2/l2.cpp
@@ -24,10 +24,20 @@ void t1()
 
 void t2()
 {
+	int b, n;
+	cout << "Enter the number n whose binomial coefficients you want to calculate\n";
+	cin >> b;
+	cout << "Your binomial coefficients" << std::endl;
+	unsigned long long Cnk = 1;
 
+	for (int k = 1; k <= b + 1; ++k)
+	{
+		cout << Cnk << " ";
+		Cnk = Cnk * (b + 1 - k) / k;
+	}
 }
 
-void t3()
+	void t3()
 {
 	int sum = 0, ctn = 0;
 	std::string line = "";
@@ -43,10 +53,10 @@ void t3()
 
 int main()
 {
-	char ebal;
+	char switcher;
 	cout << "Enter number of program (1-3): ";
-	cin >> ebal;
-		switch (ebal)
+	cin >> switcher;
+		switch (switcher)
 			{
 			case '1':
 				t1();
@@ -55,8 +65,8 @@ int main()
 			case '3':
 				t3();
 			default:
-				cout << "Ebal tvou mat suka" << '\n';
+				cout << "Error" << '\n';
 			}
 
-	return 0;
+		return 0;
 }
